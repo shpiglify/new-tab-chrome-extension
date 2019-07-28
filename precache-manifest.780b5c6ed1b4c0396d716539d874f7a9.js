77accbd7f07afc534d429229a1a104157e316350
guy20056 <guy20056@walla.com>
2019-07-28 22:55:45 +0300

    Updates

---
+++ precache-manifest.780b5c6ed1b4c0396d716539d874f7a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3886eb2091265178393",
+    "url": "build/2.e3886eb2.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "edcc80205f2178bb3857",
+    "url": "build/main.edcc8020.js"
+  },
+  {
+    "revision": "8e52b5746fdfa54c8d101dbceb27a4c5",
+    "url": "index.html"
+  },
+  {
+    "revision": "edcc80205f2178bb3857",
+    "url": "static/css/main.443cc775.chunk.css"
+  },
+  {
+    "revision": "981bb2c17f59d05bafc5db8c8e6c9d9b",
+    "url": "static/media/bing-logo.981bb2c1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.2b5f7df998b68f111bc8363207255253.js"
+  "precache-manifest.780b5c6ed1b4c0396d716539d874f7a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/new-tab-chrome-extension/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
