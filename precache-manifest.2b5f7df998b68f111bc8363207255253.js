c3f336a0a092033971df8b8d949ae14d7154d40e
guy20056 <guy20056@walla.com>
2019-07-28 22:08:45 +0300

    Updates

---
+++ precache-manifest.2b5f7df998b68f111bc8363207255253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3886eb2091265178393",
+    "url": "build/2.e3886eb2.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "472159a927fd8f5b1b5b",
+    "url": "build/main.472159a9.js"
+  },
+  {
+    "revision": "a28068d068fadf445986043df0a158a5",
+    "url": "index.html"
+  },
+  {
+    "revision": "472159a927fd8f5b1b5b",
+    "url": "static/css/main.443cc775.chunk.css"
+  },
+  {
+    "revision": "981bb2c17f59d05bafc5db8c8e6c9d9b",
+    "url": "static/media/bing-logo.981bb2c1.png"
+  }
+]);
